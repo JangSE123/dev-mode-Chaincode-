1e4e736b97b51a3f98f0215e6d2654c405bd94e6
JangSE123 <jse010206@gmail.com>
2024-03-23 13:29:34 +0900

    abstore.go change

--- chaincode/abstore/go/abstore.go
+++ chaincode/abstore/go/abstore.go
@@ -3,55 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-<<<<<<< HEAD
-	"strconv"
-
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-)
-
-// RealEstateChaincode 구조체 정의
-type RealEstateChaincode struct {
-	contractapi.Contract
-}
-
-// 소유주 등록 함수
-func (t *RealEstateChaincode) CreateOwner(ctx contractapi.TransactionContextInterface, ownerID string) error {
-	// TODO: 구현
-	return nil
-}
-
-// 구매자 등록 함수
-func (t *RealEstateChaincode) CreateCustomer(ctx contractapi.TransactionContextInterface, customerID string) error {
-	// TODO: 구현
-	return nil
-}
-
-// 건물 소유권 등록 함수
-func (t *RealEstateChaincode) CreateBuildingOwnership(ctx contractapi.TransactionContextInterface, ownerID, buildingID string) error {
-	// TODO: 구현
-	return nil
-}
-
-// 건물 소유권 조회 함수
-func (t *RealEstateChaincode) ViewBuildingOwnership(ctx contractapi.TransactionContextInterface, buildingID string) (string, error) {
-	// TODO: 구현
-	return "", nil
-}
-
-// 건물 소유권 교환 함수
-func (t *RealEstateChaincode) TradeBuildingOwnership(ctx contractapi.TransactionContextInterface, buyerID, buildingID string) error {
-	// TODO: 구현
-	return nil
-}
-
-func main() {
-	cc, err := contractapi.NewChaincode(new(RealEstateChaincode))
-	if err != nil {
-		panic(err.Error())
-	}
-	if err := cc.Start(); err != nil {
-		fmt.Printf("Error starting RealEstateChaincode: %s", err)
-=======
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -163,7 +114,6 @@ func (t *ABstore) TransferPropertyOwnership(ctx contractapi.TransactionContextIn
 	err = json.Unmarshal(propertyBytes, &property)
 	if err != nil {
 		return err
->>>>>>> 5bab1385e2c6b2598e48f5ebf1d48e820fb27eb4
 	}
 
 	property.OwnerID = newOwnerID
