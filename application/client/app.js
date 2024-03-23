280b6223f7f6c39251f04d3c7b3539ab1e456ac4
JangSE123 <jse010206@gmail.com>
2024-03-23 16:35:52 +0900

    change all

--- application/client/app.js
+++ application/client/app.js
@@ -2,101 +2,48 @@
 
 var app = angular.module('application', []);
 
-app.controller('AppCtrl', function($scope, appFactory){
-   $("#success_init").hide();
-   $("#success_qurey").hide();
-   $scope.initAB = function(){
-       appFactory.initAB($scope.abstore, function(data){
-           if(data == "")
-           $scope.init_ab = "success";
-           $("#success_init").show();
-       });
-   }
-   $scope.queryAB = function(){
-       appFactory.queryAB($scope.walletid, function(data){
-           $scope.query_ab = data;
-           $("#success_qurey").show();
-       });
-   }
-});
-app.factory('appFactory', function($http){
-      
-    var factory = {};
- 
-    factory.initAB = function(data, callback){
-        $http.get('/init?a='+data.a+'&aval='+data.aval+'&b='+data.b+'&bval='+data.bval+'&c='+data.c+'&cval='+data.cval).success(function(output){
-            callback(output)
-        });
-    }
-    factory.queryAB = function(name, callback){
-        $http.get('/query?name='+name).success(function(output){
-            callback(output)
-        });
-    }
-    return factory;
- });
- // AppCtrl 컨트롤러 수정
-app.controller('AppCtrl', function($scope, appFactory){
-    // 기존 함수 유지
-    $scope.initAB = function(){
-        appFactory.initAB($scope.abstore, function(data){
-            $scope.init_ab = "Initialization Success";
-            $("#success_init").show();
-        });
-    };
-    $scope.queryAB = function(){
-        appFactory.queryAB($scope.walletid, function(data){
-            $scope.query_ab = data;
-            $("#success_qurey").show();
+app.controller('AppCtrl', function($scope, appFactory) {
+    // 사용자 등록 함수
+    $scope.registerUser = function() {
+        var user = {
+            userID: $scope.user.userID,
+            name: $scope.user.name,
+            buildingName: $scope.user.buildingName,
+            description: $scope.user.description
+        };
+        appFactory.registerUser(user, function(data) {
+            $scope.registerUserResult = data;
         });
     };
 
-    // 사용자 등록 함수 추가
-    $scope.registerUser = function(){
-        appFactory.registerUser($scope.user, function(data){
-            $scope.register_user_result = data;
-        });
-    };
-
-    // 부동산 등록 함수 추가
-    $scope.registerProperty = function(){
-        appFactory.registerProperty($scope.property, function(data){
-            $scope.register_property_result = data;
+    // 부동산 조회 함수
+    $scope.viewBuilding = function() {
+        appFactory.viewBuilding($scope.buildingName, function(data) {
+            $scope.buildingInfo = data;
         });
     };
 });
 
-// appFactory 팩토리 수정
-app.factory('appFactory', function($http){
+app.factory('appFactory', function($http) {
     var factory = {};
 
-    // 기존 함수 유지
-    factory.initAB = function(data, callback){
-        $http.get('/init?a='+data.a+'&aval='+data.aval+'&b='+data.b+'&bval='+data.bval+'&c='+data.c+'&cval='+data.cval).success(function(output){
-            callback(output);
-        });
-    };
-    factory.queryAB = function(name, callback){
-        $http.get('/query?name='+name).success(function(output){
-            callback(output);
+    // 사용자 등록을 위한 HTTP POST 요청 함수
+    factory.registerUser = function(user, callback) {
+        $http.post('/registerUser', user).then(function(response) {
+            callback(response.data);
+        }, function(error) {
+            callback({ error: error });
         });
     };
 
-    // 사용자 등록을 위한 HTTP POST 요청 함수 추가
-    factory.registerUser = function(user, callback){
-        $http.post('/registerUser', user).success(function(output){
-            callback(output);
-        });
-    };
-
-    // 부동산 등록을 위한 HTTP POST 요청 함수 추가
-    factory.registerProperty = function(property, callback){
-        $http.post('/registerProperty', property).success(function(output){
-            callback(output);
+    // 부동산 조회를 위한 HTTP GET 요청 함수
+    factory.viewBuilding = function(buildingName, callback) {
+        $http.get('/viewBuilding?buildingName=' + buildingName).then(function(response) {
+            callback(response.data);
+        }, function(error) {
+            callback({ error: error });
         });
     };
 
     return factory;
 });
-
- 
--- application/rest/server.js
+++ application/rest/server.js
@@ -1,71 +1,31 @@
 const express = require('express');
 const app = express();
 let path = require('path');
-let sdk = require('./sdk');
+const sdk = require('./sdk');
 
 const PORT = 8001;
 const HOST = '0.0.0.0';
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/init', function (req, res) {
-   let a = req.query.a;
-   let aval = req.query.aval;
-   let b = req.query.b;
-   let bval = req.query.bval;
-   let c = req.query.c;
-   let cval = req.query.cval;
-   let args = [a, aval, b, bval, c, cval];
-   sdk.send(false, 'Init', args, res);
-});
-
-app.get('/query', function (req, res) {
-   let name = req.query.name;
-   let args = [name];
-   sdk.send(true, 'Query', args, res);
-});
-
+// 사용자 등록
 app.post('/registerUser', function (req, res) {
    let userID = req.body.userID;
    let name = req.body.name;
-   let args = [userID, name];
+   let buildingName = req.body.buildingName;
+   let description = req.body.description;
+   let args = [userID, name, buildingName, description];
    sdk.send(false, 'RegisterUser', args, res);
 });
 
-// 부동산 등록
-app.post('/registerProperty', function (req, res) {
-   let propertyID = req.body.propertyID;
-   let ownerID = req.body.ownerID;
-   let description = req.body.description;
-   let args = [propertyID, ownerID, description];
-   sdk.send(false, 'RegisterProperty', args, res);
-});
-
-// 부동산 정보 업데이트
-app.post('/updatePropertyDescription', function (req, res) {
-   let propertyID = req.body.propertyID;
-   let newDescription = req.body.newDescription;
-   let args = [propertyID, newDescription];
-   sdk.send(false, 'UpdatePropertyDescription', args, res);
-});
-
 // 부동산 조회
-app.get('/queryProperty', function (req, res) {
-   let propertyID = req.query.propertyID;
-   let args = [propertyID];
-   sdk.send(true, 'QueryProperty', args, res);
-});
-
-// 소유권 이전
-app.post('/transferPropertyOwnership', function (req, res) {
-   let propertyID = req.body.propertyID;
-   let newOwnerID = req.body.newOwnerID;
-   let args = [propertyID, newOwnerID];
-   sdk.send(false, 'TransferPropertyOwnership', args, res);
+app.get('/viewBuilding', function (req, res) {
+   let buildingName = req.query.buildingName;
+   let args = [buildingName];
+   sdk.send(true, 'ViewBuilding', args, res);
 });
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-
